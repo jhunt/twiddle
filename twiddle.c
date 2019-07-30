5bc18194bf80560472af9465909f0c2c24e760bd
James Hunt <james@niftylogic.com>
2019-07-29 21:33:00 -0400

    Step 2 - Structure for Opcode Interpretation

--- twiddle.c
+++ twiddle.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+/* opcodes for virtual machine */
+#define END  0
+#define HALT 1
+
+struct vm {
+	int  ip;
+	int *code;
+};
+
 int main(int argc, char **argv) {
 	fprintf(stderr, "Twiddle: a stack VM for language discovery\n");
+
+	int prog[] = {
+		HALT,
+		END,
+	};
+
+	struct vm vm;
+	vm.code = prog;
 	return 0;
 }
