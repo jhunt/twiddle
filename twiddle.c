95d8110dbf50548c31fb5d929d6a6bf6afab3c4f
James Hunt <james@niftylogic.com>
2019-07-29 21:33:26 -0400

    Step 3 - The Halting Program

--- twiddle.c
+++ twiddle.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/* flags for run() */
+#define TRACE   1
+#define NOTRACE 0
+
 /* opcodes for virtual machine */
 #define END  0
 #define HALT 1
@@ -8,6 +12,23 @@ struct vm {
 	int  ip;
 	int *code;
 };
+
+void run(struct vm *vm, int trace) {
+	int op;
+
+	vm->ip = 0;
+	while (op = vm->code[vm->ip]) {
+		if (trace) {
+			fprintf(stderr, "[%04x] %04x\n", vm->ip, op);
+		}
+
+		vm->ip++;
+		switch (op) {
+		case HALT:
+			return;
+		}
+	}
+}
 
 int main(int argc, char **argv) {
 	fprintf(stderr, "Twiddle: a stack VM for language discovery\n");
@@ -19,5 +40,6 @@ int main(int argc, char **argv) {
 
 	struct vm vm;
 	vm.code = prog;
+	run(&vm, TRACE);
 	return 0;
 }
